b4bed87f35137c929cfd11c4c09e2c7f1912bf74
윤정인 <yoonjeongin@naver.com>
2021-09-02 05:54:45 +0900

    input status

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
+import InputSample from './InputSample';
 import './App.css';
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
       <div className="gray-box"></div>
     </Wrapper>
     <Counter/>
+    <InputSample/>
     </>
   )
 }
