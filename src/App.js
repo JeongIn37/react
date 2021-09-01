286b0aaf9f97f185ca05afda69eb79502e1ed564
윤정인 <yoonjeongin@naver.com>
2021-09-01 23:25:01 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Hello from './Hello';
+import Wrapper from './Wrapper';
+import Counter from './Counter';
 import './App.css';
 
 function App() {
@@ -11,13 +13,15 @@ function App() {
     padding: '1rem' //다른 단위 사용시 문자열로 설정
   }
 
-  return (
+  return ( 
     <>
-      {/* 주석 안 보임 */}
-      /* 주석 보임 */
-      <Hello/>
+    <Wrapper>
+      <Hello name="react" color="red" isSpecial={true}/>
+      <Hello color="pink"/>
       <div style={style}>{name}</div>
       <div className="gray-box"></div>
+    </Wrapper>
+    <Counter/>
     </>
   )
 }
