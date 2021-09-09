cff00085942ca65ad5d2a619f9bc4ea0ffc0c957
윤정인 <yoonjeongin@naver.com>
2021-09-09 22:47:24 +0900

    using array-01

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserList from './UserList';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <UserList />
   </React.StrictMode>,
   document.getElementById('root')
 );
